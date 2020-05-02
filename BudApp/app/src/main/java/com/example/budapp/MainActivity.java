c10c740762e59d4e2100bbe2f4aa4c498f3e2c6b
MedRoy <mederic.royer@efrei.net>
2020-05-02 18:30:29 +0200

    Changement dans le fichier AndroidManifest.xml pour que l'application se lance sur l'activité MainActivity et donc ajout d'un bouton pour accéder à l'activité concernant le compte

--- BudApp/app/src/main/java/com/example/budapp/MainActivity.java
+++ BudApp/app/src/main/java/com/example/budapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.budapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mon_compte = (Button) findViewById(R.id.button_myAccount);
+        mon_compte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MyAccount.class);
+                startActivity(startIntent);
+            }
+        });
+
     }
 }
